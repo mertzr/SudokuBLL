09983c081613be852cd28567e4f77474378dac88
Kenny <Kenny@128.4.107.32>
2018-09-17 22:05:56 -0400

    I hope this works...

--- SudokuBLL/src/main/java/pkgHelper/doesElementExist.java
+++ SudokuBLL/src/main/java/pkgHelper/doesElementExist.java
@@ -2,6 +2,10 @@ package pkgHelper;
 public class doesElementExist {
 	
 	public boolean doesElementExist (int[] arr, int iValue) {
+		
+		if (arr == null) {
+			return false;
+		}
 		for (int i=0; i<arr.length; i++)
 			if (arr[i] == iValue)
 				return true;
--- SudokuBLL/src/test/java/pkgHelper/LatinSquareTest.java
+++ SudokuBLL/src/test/java/pkgHelper/LatinSquareTest.java
@@ -7,27 +7,84 @@ import org.junit.Test;
 public class LatinSquareTest {
 
 	@Test
-	public void ContainsZeroTest() {
-		int[][] myArray = {{1,2,3}, {2,3,1}, {3,1,2}};
+	public void hasDuplicates_test1() {
+		int[] arr = { 1, 2, 3, 4, 5 };
+		LatinSquare lq = new LatinSquare();
+
+		assertFalse(lq.hasDuplicates(arr));
 		
+		arr = { 1, 1, 3, 4, 5 };
+
+		assertTrue(lq.hasDuplicates(arr));
+		
+		arr = { 1, 1, 3, 4, 1 };
+
+		assertTrue(lq.hasDuplicates(arr));
+		
+		arr = null;
+
+		assertFalse(lq.hasDuplicates(arr));
+
+	}
+
+	@Test
+	public void ContainsZeroTest1() {
+		int[][] myArray = { { 1, 2, 3 }, { 2, 3, 1 }, { 3, 1, 2 } };
+
 		LatinSquare ls = new LatinSquare(myArray);
-		
-		System.out.println(ls.ContainsZero());
-		
+
+		//System.out.println(ls.ContainsZero());
+
 		assertFalse(ls.ContainsZero());
 		
 		myArray[0][1] = 0;
-		
+
 		assertTrue(ls.ContainsZero());
-		
-		int[] myIntArray = {1,2,3,4,5};
+	}
+
+	@Test
+	public void doesElementExistTest() {
+
+		int[] myIntArray = { 1, 2, 3, 4, 5 };
 		int iValue = 4;
 		
-		assertTrue(ls.doesElementExist(myIntArray,  iValue));
-		
-		iValue = 9;
-		assertFalse(ls.doesElementExist(myIntArray,  iValue));
-	}
-	
+		LatinSquare lq = new LatinSquare(myIntArray);
 
+		assertTrue(lq.doesElementExist(myIntArray, iValue));
+
+		iValue = 9;
+		assertFalse(lq.doesElementExist(myIntArray, iValue));
+		
+		myIntArray = null;
+
+		assertFalse(lq.doesElementExist(myIntArray, iValue));
+
+	}
+
+	@Test
+	public void isLatinSquare() {
+		int[][] myArray1 = { { 1, 2, 3 }, { 2, 3, 1 }, { 3, 1, 2 } };
+
+		LatinSquare lq = new LatinSquare(myArray1);
+
+		assertTrue(lq.isLatinSquare());
+		
+		int[][] myArray2 = { { 2, 2, 3 }, { 2, 3, 1 }, { 3, 1, 2 } };
+
+		LatinSquare lq = new LatinSquare(myArray2);
+
+		assertFalse(lq.isLatinSquare());
+		
+		int[][] myArray3 = { { 1, 2, 3 }, { 2, 3, 1 }, { 2, 1, 2 } };
+
+		LatinSquare lq = new LatinSquare(myArray3);
+
+		assertFalse(lq.isLatinSquare());
+		
+		int[][] myArray4 = { { 1, 2, 3 }, { 3, 2, 1 }, { 3, 1, 2 } };
+
+		LatinSquare lq = new LatinSquare(myArray4);
+
+		assertTrue(lq.isLatinSquare());
+	}
 }

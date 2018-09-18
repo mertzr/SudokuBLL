11e058292d77c2c5c7f35f52cf71b1f8f04cfd40
Mohammex <42977749+Mohammex@users.noreply.github.com>
2018-09-17 23:22:44 -0400

    Update LatinSquare.java

--- SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
+++ SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
@@ -25,6 +25,24 @@ public class LatinSquare {
 		
 	}
 	
+	public boolean isLatinSquare() {
+		
+		int firstRow = getRow(0);
+		boolean noDuplicateBool = true;
+		
+		if (hasAllValues(firstRow)) {
+			for (int counter = 1, aRow = getRow(counter); ((counter <= getRow(counter).length) || (noDuplicateBool == true)) ; counter ++) {
+				if (hasDuplicates(firstRow, aRow)) {
+					noDuplicateBool = false;
+					return false;
+				}
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	
 	public boolean ContainsZero()
 	{
 		

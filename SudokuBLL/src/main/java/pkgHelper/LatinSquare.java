15b2039020335d1c6088557a1d8f34ebc3d69698
Mohammex <42977749+Mohammex@users.noreply.github.com>
2018-09-17 23:19:27 -0400

    Update LatinSquare.java

--- SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
+++ SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
@@ -13,6 +13,18 @@ public class LatinSquare {
 		return this.LatinSquare;
 	}
 	
+	public boolean doesElementExist (int[] arr, int iValue) {
+		
+		if (arr == null) {
+			return false;
+		}
+		for (int i=0; i<arr.length; i++)
+			if (arr[i] == iValue)
+				return true;
+		return false;
+		
+	}
+	
 	public boolean ContainsZero()
 	{
 		

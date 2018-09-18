7ef0eb94cc1538460ef4a1c259f731f172700a6a
Mohammed Alshuwaykhat <Mohammex@udel.edu>
2018-09-17 20:11:39 -0400

    Bug fix

--- SudokuBLL/src/main/java/pkgHelper/doesElementExist.java
+++ SudokuBLL/src/main/java/pkgHelper/doesElementExist.java
@@ -4,7 +4,7 @@ public class doesElementExist {
 	public boolean doesElementExist (int[] arr, int iValue) {
 		for (int i=0; i<arr.length; i++)
 			if (arr[i] == iValue)
-				return false;
-		return true;
+				return true;
+		return false;
 	}
 }

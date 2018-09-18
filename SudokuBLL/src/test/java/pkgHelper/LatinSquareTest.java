e35531fac8a34b2e08a1f79cc00ee65c0376966b
Kenny <Kenny@128.4.107.32>
2018-09-17 23:05:57 -0400

    sample2 + LatinSquareTest combo

--- SudokuBLL/src/test/java/pkgHelper/LatinSquareTest.java
+++ SudokuBLL/src/test/java/pkgHelper/LatinSquareTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pkgTest.LatinSquare;
+
 public class LatinSquareTest {
 
 	@Test
@@ -12,17 +14,17 @@ public class LatinSquareTest {
 		LatinSquare lq = new LatinSquare();
 
 		assertFalse(lq.hasDuplicates(arr));
-		
+
 		int[] arr2 = { 1, 1, 3, 4, 5 };
 		LatinSquare lq = new LatinSquare();
 
 		assertTrue(lq.hasDuplicates(arr));
-		
+
 		int[] arr3 = { 1, 1, 3, 4, 1 };
 		LatinSquare lq = new LatinSquare();
 
 		assertTrue(lq.hasDuplicates(arr));
-		
+
 		int[] arr4 = null;
 		LatinSquare lq = new LatinSquare();
 
@@ -35,10 +37,10 @@ public class LatinSquareTest {
 
 		LatinSquare ls = new LatinSquare(myArray);
 
-		//System.out.println(ls.ContainsZero());
+		// System.out.println(ls.ContainsZero());
 
 		assertFalse(ls.ContainsZero());
-		
+
 		myArray[0][1] = 0;
 
 		assertTrue(ls.ContainsZero());
@@ -49,14 +51,14 @@ public class LatinSquareTest {
 
 		int[] myIntArray = { 1, 2, 3, 4, 5 };
 		int iValue = 4;
-		
+
 		LatinSquare lq = new LatinSquare(myIntArray);
 
 		assertTrue(lq.doesElementExist(myIntArray, iValue));
 
 		iValue = 9;
 		assertFalse(lq.doesElementExist(myIntArray, iValue));
-		
+
 		myIntArray = null;
 
 		assertFalse(lq.doesElementExist(myIntArray, iValue));
@@ -69,23 +71,81 @@ public class LatinSquareTest {
 		LatinSquare lq = new LatinSquare(myArray1);
 
 		assertTrue(lq.isLatinSquare());
-		
+
 		int[][] myArray2 = { { 2, 2, 3 }, { 2, 3, 1 }, { 3, 1, 2 } };
 
 		LatinSquare lq = new LatinSquare(myArray2);
 
 		assertFalse(lq.isLatinSquare());
-		
+
 		int[][] myArray3 = { { 1, 2, 3 }, { 2, 3, 1 }, { 2, 1, 2 } };
 
 		LatinSquare lq = new LatinSquare(myArray3);
 
 		assertFalse(lq.isLatinSquare());
-		
+
 		int[][] myArray4 = { { 1, 2, 3 }, { 3, 2, 1 }, { 3, 1, 2 } };
 
 		LatinSquare lq = new LatinSquare(myArray4);
 
 		assertTrue(lq.isLatinSquare());
 	}
-}
+
+	@Test
+	public void hasAllValues1() {
+		int[] arr1 = { 1, 3, 5, 7, 9 };
+		int[] arr2 = { 1, 3, 5, 7, 9 };
+		LatinSquare lq = new LatinSquare();
+
+		assertTrue(lq.hasAllValues(arr1, arr2));
+	}
+
+	@Test
+	public void hasAllValues2() {
+		int[] arr1 = { 1, 3, 5, 7, 9 };
+		int[] arr2 = { 1, 3, 5, 8, 9 };
+		LatinSquare lq = new LatinSquare();
+
+		assertTrue(lq.hasAllValues(arr1, arr2));
+	}
+
+	@Test
+	public boolean hasAllValues(int[] arr1, int[] arr2) {
+
+		if (arr1 == arr2) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Test
+	public int[] getColumnTest(int iCol) {
+
+		int[][] square = { { 1, 2, 3 }, { 2, 3, 1 }, { 3, 1, 2 } };
+		int[][] square2 = { { 1, 2, 5 }, { 2, 7, 1 }, { 3, 3, 2 } };
+
+		int[] result = new int[9];
+
+		for (int i = 0; i < 9; i++) {
+			result[i] = square[i][iCol];
+		}
+
+		return result;
+	}
+
+	@Test
+		public void getRowTest() {
+	
+		int[][] square1 = {{1,2,3}, {2,3,1}, {3,1,2}};
+		int[][] square2 = {{1,7,3}, {2,2,1}, {3,8,2}};
+		
+		int[] result1 = square1[1];
+		int[] result2 = square2()[0];
+		
+		int[] r1 = {2,3,1};
+		int[] r2 = {1,7,3};
+		
+		assertTrue("getRow works", (result1 == r1));
+		assertTrue("getRow works", (result2 == r2));
+	}

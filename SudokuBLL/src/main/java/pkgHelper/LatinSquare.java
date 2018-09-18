c93956439549162ca30032ab2c1c62ed7a07d21d
Mohammex <42977749+Mohammex@users.noreply.github.com>
2018-09-17 23:16:40 -0400

    Update LatinSquare.java

--- SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
+++ SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
@@ -32,5 +32,16 @@ public class LatinSquare {
 		return false;
 	}
 	
-	
+	public int[] getColumn(int iCol) {
+		
+		int[][] square = getLatinSquare();
+		int[] result = new int[9];
+		
+		for (int i=0; i<9; i++) {
+			result[i] = square[i][iCol];
+		}
+		
+		return result;
+		
+	}
 }
